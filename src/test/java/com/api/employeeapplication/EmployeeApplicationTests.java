4eb8ef27380400ebe11d665565202cc556339cc4
Harry S <harry.smith@fdmgroup.com>
2023-08-17 11:24:19 -0400

    spring intializer add

---
+++ src/test/java/com/api/employeeapplication/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.employeeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
