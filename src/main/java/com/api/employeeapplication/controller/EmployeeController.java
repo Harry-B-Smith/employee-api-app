41a0bdb6a3f04214b18f79c36c73fd1defd33672
Harry S <harry.smith@fdmgroup.com>
2023-08-18 16:48:30 -0400

    add getAllEmployees method in controller, add cross origin for angular later

--- src/main/java/com/api/employeeapplication/controller/EmployeeController.java
+++ src/main/java/com/api/employeeapplication/controller/EmployeeController.java
@@ -1,17 +1,24 @@
 package com.api.employeeapplication.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.employeeapplication.entity.Employee;
 import com.api.employeeapplication.service.EmployeeService;
 
 @RestController
-@RequestMapping("employee")
+@RequestMapping("/employee")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeController {
 	
-	EmployeeService employeeService;
+	@Autowired
+	private EmployeeService employeeService;
 
 	@Autowired
 	public EmployeeController(EmployeeService employeeService) {
@@ -19,6 +26,13 @@ public class EmployeeController {
 		this.employeeService = employeeService;
 	}
 	
+	@GetMapping
+	public List<Employee> getAllEmployees() {
+		
+		List<Employee> employees = employeeService.getAllEmployees();
+		return employees;
+	}
+
 	
 
 }
--- src/main/java/com/api/employeeapplication/service/EmployeeService.java
+++ src/main/java/com/api/employeeapplication/service/EmployeeService.java
@@ -1,8 +1,11 @@
 package com.api.employeeapplication.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.api.employeeapplication.entity.Employee;
 import com.api.employeeapplication.repo.EmployeeRepository;
 
 @Service
@@ -14,6 +17,11 @@ public class EmployeeService {
 	public EmployeeService(EmployeeRepository employeeRepo) {
 		super();
 		this.employeeRepo = employeeRepo;
+	}
+
+	public List<Employee> getAllEmployees() {
+		
+		return employeeRepo.findAll();
 	}
 	
 
