71b192ab5523dac779fdaa41e878dc6ce26c6197
Harry S <harry.smith@fdmgroup.com>
2023-08-18 13:03:36 -0400

    add Employee Controller functionalit

--- src/main/java/com/api/employeeapplication/controller/EmployeeController.java
+++ src/main/java/com/api/employeeapplication/controller/EmployeeController.java
@@ -1,5 +1,24 @@
 package com.api.employeeapplication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.employeeapplication.service.EmployeeService;
+
+@RestController
+@RequestMapping("employee")
 public class EmployeeController {
+	
+	EmployeeService employeeService;
+
+	@Autowired
+	public EmployeeController(EmployeeService employeeService) {
+		super();
+		this.employeeService = employeeService;
+	}
+	
+	
 
 }
