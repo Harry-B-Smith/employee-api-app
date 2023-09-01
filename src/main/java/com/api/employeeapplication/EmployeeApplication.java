ab3850e9d43ad3bb8e206b2b1831e5a036729b80
Harry <harry.iv@outlook.com>
2023-09-01 13:54:30 -0400

    customized swagger configuration

--- src/main/java/com/api/employeeapplication/EmployeeApplication.java
+++ src/main/java/com/api/employeeapplication/EmployeeApplication.java
@@ -1,9 +1,18 @@
 package com.api.employeeapplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Employee API App",
+				description = "A consumable API, allows for complete CRUD operations"
+		)
+)
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
